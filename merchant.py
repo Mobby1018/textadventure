0304400e5ac0dee000ced9b856b6344d72f62844
Mobby1018 <alexhehner4@gmail.com>
2024-04-24 14:45:21 -0500

    Game Complete

--- merchant.py
+++
@@ -1,6 +0,0 @@
-import items, world
-
-class Merchant:
-    def __init__(self, name, inv):
-        self.inv = [items.SmallHealthPotion(), items.LargeHealthPotion(), items.MysteryPotion()]
-        self.location_x, self.location_y = world.starting_position
